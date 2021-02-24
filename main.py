c417766903be1d6d24f52d518b550f75b5a87036
Nemanja Lakicevic <n@lakicevic.no>
2021-02-24 14:09:51 +0100

    adding optimization for task 1. not finished solution
    not working as intended
    
    test opt works

---
+++ main.py
@@ -0,0 +1,17 @@
+from objects import ElAppliance, Household, Neighborhood
+
+if __name__ == "__main__":
+    #                  0 , 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23
+    costPerHourToU = [0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5, 1 , 1 , 1 , 1 ,0.5,0.5,0.5]
+    myNeighborhood = Neighborhood("Blindern","ToU")
+
+    myHouse = Household("test")
+    myHouse.elAppliance.append(ElAppliance("Dishwasher", 1.44, 1.44, 1, 0, 23))
+    myHouse.elAppliance.append(ElAppliance("EV", 9.9, 9.9, 3, 0, 23))
+    myHouse.elAppliance.append(ElAppliance("WashingMachine", 1.94, 1.94, 1, 0, 23))
+
+    myNeighborhood.houses.append(myHouse)
+
+    for x in range(len(myNeighborhood.houses)):
+        myNeighborhood.printInfo(x)
+
--- objects.py
+++ objects.py
@@ -1,7 +1,7 @@
 import random
 
 
-class ElApplience:
+class ElAppliance:
     def __init__(self,name,dailyUsageMin,dailyUsageMax,duration,timeMin = 0,timeMax = 23):
         self.name = name
         self.dailyUsageMin = dailyUsageMin
@@ -24,11 +24,15 @@ class ElApplience:
         return random.randint(self.dailyUsageMin, (self.dailyUsageMax-self.duration))
 
 class Household:
-    elAppliences = []
+    elAppliance = []
 
-    def __init__(self,name):
+    def __init__(self, name, appliances=None):
         #inizalise
         self.name = name
+        if appliances is not None:
+            for a in appliances:
+                self.elAppliance.append(a)
+
 
 class Neighborhood:
     dailyPowerTimetable =[]
@@ -50,31 +54,16 @@ class Neighborhood:
         self.updateTimetable(priceScheme)
 
     def printInfo(self,houseNumber):
-        for x in range(len(self.houses[houseNumber].elAppliences)):
-            print(self.houses[houseNumber].elAppliences[x].name)
-            print("Timeframe start:", (self.houses[houseNumber].elAppliences[x].timeMin+1))
-            print("Timeframe end:", (self.houses[houseNumber].elAppliences[x].timeMax+1))
-            randomTime = self.houses[houseNumber].elAppliences[x].randomTime()
+        for x in range(len(self.houses[houseNumber].elAppliance)):
+            print(self.houses[houseNumber].elAppliance[x].name)
+            print("Timeframe start:", (self.houses[houseNumber].elAppliance[x].timeMin+1))
+            print("Timeframe end:", (self.houses[houseNumber].elAppliance[x].timeMax+1))
+            randomTime = self.houses[houseNumber].elAppliance[x].randomTime()
             print("Actual start-time:", randomTime)
-            print("Operating time in hours:", self.houses[houseNumber].elAppliences[x].duration)
+            print("Operating time in hours:", self.houses[houseNumber].elAppliance[x].duration)
             pricePerKWH = self.dailyPowerTimetable[randomTime]
-            print("Total energy usage (max):", self.houses[houseNumber].elAppliences[x].dailyUsageMax)
+            print("Total energy usage (max):", self.houses[houseNumber].elAppliance[x].dailyUsageMax)
             print("Energy price per kWh:", pricePerKWH)
-            print("Energy price in total:", pricePerKWH * self.houses[houseNumber].elAppliences[x].dailyUsageMax, "\n")
+            print("Energy price in total:", pricePerKWH * self.houses[houseNumber].elAppliance[x].dailyUsageMax, "\n")
 
 
-if __name__ == "__main__":
-    #                  0 , 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23
-    costPerHourToU = [0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5, 1 , 1 , 1 , 1 ,0.5,0.5,0.5]
-    myNeighborhood = Neighborhood("Blindern","ToU")
-
-    myHouse = Household("test")
-    myHouse.elAppliences.append(ElApplience("Dishwasher", 1.44, 1.44, 1, 0, 23))
-    myHouse.elAppliences.append(ElApplience("EV", 9.9, 9.9, 3, 0, 23))
-    myHouse.elAppliences.append(ElApplience("WashingMachine", 1.94, 1.94, 1, 0, 23))
-
-    myNeighborhood.houses.append(myHouse)
-
-    for x in range(len(myNeighborhood.houses)):
-        myNeighborhood.printInfo(x)
-    
