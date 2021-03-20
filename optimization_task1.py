c3d3d0ec615882c40ee15e30f14538c6e112655f
Nemanja Lakicevic <n@lakicevic.no>
2021-03-20 19:11:06 +0100

    Neighbourhood works

--- optimization_task1.py
+++ optimization_task1.py
@@ -1,5 +1,6 @@
+import copy
 import numpy as np
-from pandas import DataFrame
+import pandas as pd
 from scipy.optimize import linprog
 from objects import ElAppliance, Household, ElType
 import price_scheduling as ps
@@ -40,7 +41,6 @@ def create_ub_constraints(appliances: list, hours=24, task4=False, peak_load=Non
         index += hours
 
     if task4:
-        print("HEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHE")
         if peak_load is None:
             raise ValueError("Input peak load in order to balance load")
         for h in range(hours):
@@ -118,20 +118,6 @@ def schedule_shiftable(shiftable_non_continious: list, hourly_prices: list, task
 
     return names, schedule
 
-
-def get_house_load_schedule_depr(house: Household, hourly_prices: list):
-    shiftable_continuous, shiftable_non_continuous, non_shiftable = sort_appliances(house.elAppliance)
-
-    ns_names, ns_schedule = schedule_non_shiftable(non_shiftable)
-    sc_names, sc_schedule = schedule_shiftable_continuous(shiftable_continuous, hourly_prices)
-    snc_names, snc_schedule = schedule_shiftable(shiftable_non_continuous, hourly_prices, task4=task4)
-
-    columns = ns_names + sc_names + snc_names
-    schedule = ns_schedule + sc_schedule + snc_schedule
-
-    return DataFrame([list(x) for x in zip(*schedule)], columns=columns)
-
-
 def get_optimal_hours_for_continuous(appliance: ElAppliance, hourly_prices):
     cost = []
     for h in range(appliance.timeMin, appliance.timeMax-appliance.duration):
@@ -164,40 +150,64 @@ def get_load_schedule(appliances: list, hourly_prices: list, task4=False, peak_l
     updated_continuous = updated_hours_continuous(shiftable_continuous, hourly_prices)
     shiftable = shiftable_non_continuous + updated_continuous
 
-    schedule, columns = None, None
+    names, schedule = None, None
 
     if task4:
-        columns, schedule = schedule_shiftable(non_shiftable+shiftable,
+        names, schedule = schedule_shiftable(non_shiftable+shiftable,
                 hourly_prices, task4=task4, peak_load=peak_load)
     else:
         ns_names, ns_schedule = schedule_non_shiftable(non_shiftable)
         snc_names, snc_schedule = schedule_shiftable(shiftable, hourly_prices)
 
-        columns = ns_names + snc_names
+        names = ns_names + snc_names
         schedule = ns_schedule + snc_schedule
 
-    return DataFrame([list(x) for x in zip(*schedule)], columns=columns)
+    return names, schedule
 
+
+def get_house_load_schedule(house, hourly_prices, task4=False, peak_load=None):
+    names, schedule = get_load_schedule(house.elAppliance, hourly_prices,
+            task4=task4, peak_load=peak_load)
+
+    return pd.DataFrame([list(x) for x in zip(*schedule)], columns=names)
 
 
 def get_neighbourhood_load_schedule(neighbourhood: list, hourly_prices):
-
     house_list = []
     optimizable = []
     for house in neighbourhood:
-        name = house.name
-        num_appliances = len(house.elAppliance)
-        print(num_appliances)
-        appliance_names = []
         for a in house.elAppliance:
-            appliance_names.append(a.name)
+            a.name = (house.name, a.name)
 
-        house_list.append((house.name, len(house.elAppliance), appliance_names))
-        optimizable += house.elAppliance
+        shiftable_continuous, shiftable_non_continuous, non_shiftable = sort_appliances(house.elAppliance)
 
-    df = get_load_schedule(optimizable, hourly_prices, task4=None, peak_load=None)
+        updated_continuous = updated_hours_continuous(shiftable_continuous, hourly_prices)
+        shiftable = shiftable_non_continuous + updated_continuous
 
-    return df
+        optimizable.extend(non_shiftable + shiftable)
+
+        #shiftable_continuous, shiftable_non_continuous, non_shiftable = sort_appliances(house.elAppliance)
+        #optimizable.extend(house.elAppliance)
+
+
+        # for a in non_shiftable:
+        #     house_list.append((house.name, a.name))
+
+        # for a in shiftable_non_continuous:
+        #     house_list.append((house.name, a.name))
+
+        # for a in shiftable_continuous:
+        #     house_list.append((house.name, a.name))
+
+    names, schedule = schedule_shiftable(optimizable, hourly_prices, task4=False,
+            peak_load=None)
+
+    print(names)
+    columns = pd.MultiIndex.from_tuples(names)
+
+    return pd.DataFrame([list(x) for x in zip(*schedule)],
+            columns=columns)
+
 
 def set_load_penalty(peak_hours: list[tuple[int, int]], penalty: float, hours=24):
     penalty_constraints = np.ones(hours)
@@ -235,8 +245,9 @@ def pick_random_appliances(appliances: list):
     return picked_appliances
 
 
-def make_neighbourhood(num_houses: int, appliances: list):
+def make_neighbourhood(num_houses: int):
     neighbourhood = []
+    appliances = make_list()
     for h in range(num_houses):
         num_appliances = random.randint(5, len(appliances)-1)
         house = Household("House_" + str(h), random.sample(appliances, num_appliances))
@@ -253,52 +264,56 @@ hourly_prices = [0.1, 0.1, 0.3, 0.2, 0.1, 0.1, 0.2, 0.1, 0.3,
         0.3, 0.3, 0.2,0.3, 0.3, 0.3, 0.2,0.3, 0.3, 0.3, 0.2,0.3, 0.3, 0.3, 0.1]
 
 
-lght = ElAppliance("Lighing", 1, 2, 0.2, 10, ElType.non_shiftable, timeMin=10, timeMax=20)
-heat = ElAppliance("Heating", 6.4, 9.6, 0.4, 24, ElType.non_shiftable, timeMin=0, timeMax=24)
-ev = ElAppliance("Electric batmobile", 9.9, 9.9, 3.3, 3, ElType.shiftable, timeMin=0, timeMax=8)
-ev2 = ElAppliance("Electric bitchmobile", 9.9, 9.9, 3.3, 3, ElType.shiftable_non_continious, timeMin=8, timeMax=16)
-stove = ElAppliance("Stove", 3.9, 3.9, 2, 3, ElType.shiftable, timeMin=14, timeMax=22)
-rfrg = ElAppliance("Refrigerator", 1.32, 3.96, 0.164, 24, ElType.non_shiftable, timeMin=0, timeMax=24)
-tv = ElAppliance("Tv", 0.15, 0.6, 0.12, 5, ElType.non_shiftable, timeMin=17, timeMax=22)
-computer = ElAppliance("Computer", 0.6, 0.6, 0.1, 6, ElType.non_shiftable, timeMin=8, timeMax=24)
-router = ElAppliance("Router", 0.14, 0.14, 0.006, 24, ElType.non_shiftable, timeMin=0,timeMax=24)
-fan = ElAppliance("Ceiling Fan", 0.22, 0.21, 0.073, 3, ElType.shiftable_non_continious, timeMin=12, timeMax=20)
-freezer = ElAppliance("Freezer", 0.84, 0.84,  0.035, 24, ElType.non_shiftable, timeMin=0, timeMax=24)
-dw = ElAppliance("Dishwaasher", 1.44, 1.44, 1.44, 1, ElType.shiftable_non_continious, timeMin=14, timeMax=24)
-lm = ElAppliance("Laundry Machine", 1.94, 1.94, 0.485, 4, ElType.shiftable, timeMin=0, timeMax=18)
-dryer = ElAppliance("Cloth Dryer", 2.5, 2.5, 2.5, 1, ElType.shiftable, timeMin=0, timeMax=20)
-iron = ElAppliance("Cloth Iron", 0.28, 0.28, 0.28, 1, ElType.shiftable, timeMin=10, timeMax=20)
-cellcharger = ElAppliance("Cellphone charger", 0.01, 0.01, 0.003, 4, ElType.shiftable_non_continious, timeMin=0, timeMax=12)
-mw = ElAppliance("Microwave", 0.6, 0.6, 0.6, 1, ElType.shiftable, timeMin=17, timeMax=24)
-hd = ElAppliance("Hair Dryer", 0.19, 0.19, 0.19, 1, ElType.shiftable, timeMin=19, timeMax=23)
-toaster = ElAppliance("Toaster", 0.3, 0.3, 0.3, 1, ElType.shiftable, timeMin=12, timeMax=20)
+def make_list():
+    lght = ElAppliance("Lighing", 1, 2, 0.2, 10, ElType.non_shiftable, timeMin=10, timeMax=20)
+    heat = ElAppliance("Heating", 6.4, 9.6, 0.4, 24, ElType.non_shiftable, timeMin=0, timeMax=24)
+    ev = ElAppliance("Electric batmobile", 9.9, 9.9, 3.3, 3, ElType.shiftable, timeMin=0, timeMax=8)
+    ev2 = ElAppliance("Electric bitchmobile", 9.9, 9.9, 3.3, 3, ElType.shiftable_non_continious, timeMin=8, timeMax=16)
+    stove = ElAppliance("Stove", 3.9, 3.9, 2, 3, ElType.shiftable, timeMin=14, timeMax=22)
+    rfrg = ElAppliance("Refrigerator", 1.32, 3.96, 0.164, 24, ElType.non_shiftable, timeMin=0, timeMax=24)
+    tv = ElAppliance("Tv", 0.15, 0.6, 0.12, 5, ElType.non_shiftable, timeMin=17, timeMax=22)
+    computer = ElAppliance("Computer", 0.6, 0.6, 0.1, 6, ElType.non_shiftable, timeMin=8, timeMax=24)
+    router = ElAppliance("Router", 0.14, 0.14, 0.006, 24, ElType.non_shiftable, timeMin=0,timeMax=24)
+    fan = ElAppliance("Ceiling Fan", 0.22, 0.21, 0.073, 3, ElType.shiftable_non_continious, timeMin=12, timeMax=20)
+    freezer = ElAppliance("Freezer", 0.84, 0.84,  0.035, 24, ElType.non_shiftable, timeMin=0, timeMax=24)
+    dw = ElAppliance("Dishwaasher", 1.44, 1.44, 1.44, 1, ElType.shiftable_non_continious, timeMin=14, timeMax=24)
+    lm = ElAppliance("Laundry Machine", 1.94, 1.94, 0.485, 4, ElType.shiftable, timeMin=0, timeMax=18)
+    dryer = ElAppliance("Cloth Dryer", 2.5, 2.5, 2.5, 1, ElType.shiftable, timeMin=0, timeMax=20)
+    iron = ElAppliance("Cloth Iron", 0.28, 0.28, 0.28, 1, ElType.shiftable, timeMin=10, timeMax=20)
+    cellcharger = ElAppliance("Cellphone charger", 0.01, 0.01, 0.003, 4, ElType.shiftable_non_continious, timeMin=0, timeMax=12)
+    mw = ElAppliance("Microwave", 0.6, 0.6, 0.6, 1, ElType.shiftable, timeMin=17, timeMax=24)
+    hd = ElAppliance("Hair Dryer", 0.19, 0.19, 0.19, 1, ElType.shiftable, timeMin=19, timeMax=23)
+    toaster = ElAppliance("Toaster", 0.3, 0.3, 0.3, 1, ElType.shiftable, timeMin=12, timeMax=20)
 
 
-appliances = [lght, heat, rfrg, stove, ev, ev2, tv, computer, router, fan,
-       freezer, dw, lm, dryer, iron, cellcharger, mw, hd, toaster]
+    appliances = [lght, heat, rfrg, stove, ev, ev2, tv, computer, router, fan,
+           freezer, dw, lm, dryer, iron, cellcharger, mw, hd, toaster]
+    return copy.deepcopy(appliances)
 
-appliances2 = [lght, heat, ev, stove, ev2]
+# appliances2 = [lght, heat, ev, stove, ev2]
 
-house = Household("Mi Casa", appliances2)
+# house = Household("Mi Casa", appliances)
 
 # load = get_total_daily_load(house)
 # print(load)
 #
-# df = get_load_schedule(house, hourly_prices)
+# df = get_house_load_schedule(house, hourly_prices)
 # print(df)
 #
-# total_hourly_load = DataFrame(df.sum(axis=1), columns=["Load kWh"])
+# total_hourly_load = pd.DataFrame(df.sum(axis=1), columns=["Load kWh"])
 # print(total_hourly_load)
 
 # Task 3
-# neighbourhood = make_neighbourhood(2, appliances)
-#
-#
-#
-# df = get_neighbourhood_load_schedule(neighbourhood, hourly_prices)
-# print(df)
+neighbourhood = make_neighbourhood(20)
 
+df = get_neighbourhood_load_schedule(neighbourhood, hourly_prices)
 
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+pd.set_option('display.max_colwidth', -1)
+
+print(df)
 # for h in neighbourhood:
 #     print(h.name)
 #     # for a in h.elAppliance:
@@ -313,8 +328,8 @@ house = Household("Mi Casa", appliances2)
 #
 
 # Task 4
-df = get_load_schedule(house.elAppliance, hourly_prices)
-print(df)
+# df = get_load_schedule(house.elAppliance, hourly_prices)
+# print(df)
 # df = get_load_schedule(house, hourly_prices, task4=True, peak_load=3.7)
 #
 # print(df)
