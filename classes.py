d2e81683765a645c6702fbc85dee16aea76c7f0d
finneaa <finneaa@student.matnat.uio.no>
2021-03-21 12:48:58 +0100

    All values tweeked
    changes to make_standard_appliances() make_aux_appliances()

--- classes.py
+++ classes.py
@@ -40,7 +40,7 @@ class Household:
         appliances.append(ElAppliance("Computer", 0.6, 0.6, 0.1, 6, ElType.non_shiftable_non_continuous, timeMin=8, timeMax=23))
         appliances.append(ElAppliance("Dishwasher", 1.44, 1.44, 1.44, 1, ElType.shiftable, timeMin=8, timeMax=17))
         appliances.append(ElAppliance("Laundry Machine", 1.94, 1.94, 0.485, 4, ElType.shiftable, timeMin=8, timeMax=22))
-        appliances.append(ElAppliance("Cloth Dryer", 2.5, 2.5, 2.5, 1, ElType.shiftable, timeMin=0, timeMax=20))
+        appliances.append(ElAppliance("Cloth Dryer", 2.5, 2.5, 2.5, 1, ElType.shiftable, timeMin=18, timeMax=22))
         #.... Resten av standarde appliances
 
         # Deeop copy for å unngå statisk dritt
@@ -63,14 +63,14 @@ class Household:
         appliances = []
 
         #appliances.append(ElAppliance("EV", 9.9, 9.9, 3.3, 3, ElType.shiftable_non_continuous, timeMin=0, timeMax=8))
-        appliances.append(ElAppliance("Ceiling Fan", 0.22, 0.21, 0.073, 3, ElType.shiftable_non_continuous, timeMin=12, timeMax=20))
+        appliances.append(ElAppliance("Ceiling Fan", 0.22, 0.21, 0.073, 3, ElType.non_shiftable_non_continuous, timeMin=11, timeMax=18))
         appliances.append(ElAppliance("Freezer", 0.84, 0.84,  0.035, 24, ElType.non_shiftable, timeMin=0, timeMax=24))
-        appliances.append(ElAppliance("Cloth Iron", 0.28, 0.28, 0.28, 1, ElType.shiftable, timeMin=10, timeMax=20))
+        appliances.append(ElAppliance("Cloth Iron", 0.28, 0.28, 0.28, 1, ElType.shiftable, timeMin=19, timeMax=22))
         appliances.append(ElAppliance("Router", 0.14, 0.14, 0.006, 24, ElType.non_shiftable, timeMin=0,timeMax=24))
-        appliances.append(ElAppliance("Cellphone charger", 0.01, 0.01, 0.003, 4, ElType.shiftable_non_continuous, timeMin=0, timeMax=12))
-        appliances.append(ElAppliance("Microwave", 0.6, 0.6, 0.6, 1, ElType.shiftable, timeMin=17, timeMax=24))
-        appliances.append(ElAppliance("Hair Dryer", 0.19, 0.19, 0.19, 1, ElType.shiftable, timeMin=19, timeMax=23))
-        appliances.append(ElAppliance("Toaster", 0.3, 0.3, 0.3, 1, ElType.shiftable, timeMin=12, timeMax=20))
+        appliances.append(ElAppliance("Cellphone charger", 0.01, 0.01, 0.003, 4, ElType.shiftable_non_continuous, timeMin=0, timeMax=8))
+        appliances.append(ElAppliance("Microwave", 0.6, 0.6, 0.6, 1, ElType.shiftable, timeMin=16, timeMax=19))
+        appliances.append(ElAppliance("Hair Dryer", 0.19, 0.19, 0.19, 1, ElType.shiftable, timeMin=8, timeMax=21))
+        appliances.append(ElAppliance("Toaster", 0.3, 0.3, 0.3, 1, ElType.shiftable, timeMin=8, timeMax=12))
         # .... Resten av aux appliances
 
         # Velg antall random appliances mellom 2 og antall appliances i lista
